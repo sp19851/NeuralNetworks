83be0ba25637a1477f103ef31fbad5e8bbb8e234
sp19851 <sp19851@gmail.com>
2025-10-19 14:52:01 +0200

    реализация нейрона

---
+++ NeuralNetworks/NeuralType.cs
@@ -0,0 +1,9 @@
+﻿namespace NeuralNetworks
+{
+    public enum NeuralType
+    {
+        Input = 0,
+        Normal = 1,
+        Output = 2
+    }
+}
---
+++ NeuralNetworks/Neuron.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NeuralNetworks
+{
+    public class Neuron
+    {
+        public List<double> Weights { get; }
+        public NeuralType NeuralType { get; }
+        public double Output { get; private set; }
+
+        public Neuron(int inputCount, NeuralType neuralType = NeuralType.Normal)
+        {
+            NeuralType = neuralType;
+            Weights = new List<double>();
+
+            for (var i = 0; i < inputCount; i++)
+            {
+                Weights.Add(1);
+            }
+        }
+
+        public double FeedForward(List<double> inputs)
+        {
+            var sum = 0.0;
+            for (var i = 0; i < inputs.Count; i++)
+            {
+                sum += Weights[i] * inputs[i];
+            }
+
+            Output = Sigmoid(sum);
+            return Output;
+        }
+
+        private double Sigmoid(double x)
+        {
+            var result = 1.0 / (1.0 + Math.Pow(Math.E, -x));
+            return result;
+        }
+
+        public override string ToString()
+        {
+            return $"Output: {Output} Neuron Type: {NeuralType}";
+        }
+    }
+}
