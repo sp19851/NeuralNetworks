82253dcd3b9f1f579f671735c16e79aed45ea297
sp19851 <sp19851@gmail.com>
2025-10-26 07:44:22 +0200

    Нейрон с механизмомо обучения

--- NeuralNetworks/Neural.cs
+++ NeuralNetworks/Neural.cs
@@ -6,26 +6,43 @@ namespace NeuralNetworks
     public class Neural
     {
         public List<double> Weights { get; }
+        //Добавляем входные сигналы для обучения
+        public List<double> Inputs { get; }
         public NeuralType NeuralType { get; }
         public double Output { get; private set; }
+        //Добавляем дельту для обучения
+        public double Delta { get; private set; }
 
         public Neural(int inputCount, NeuralType neuralType = NeuralType.Hidden)
         {
             NeuralType = neuralType;
             Weights = new List<double>();
-            //входной слой всеггда имеет вес 1
+            Inputs = new List<double>();
+            //входной слой всегда имеет вес 1
+            //инициализируем веса случайными числами
+            InitWeightRandomValues(inputCount);
+        }
+
+        private void InitWeightRandomValues(int inputCount)
+        {
+            var rnd = new Random();
             for (var i = 0; i < inputCount; i++)
             {
-                Weights.Add(1);
+                Weights.Add(rnd.NextDouble());
+                Inputs.Add(0);
             }
         }
 
         public double FeedForward(List<double> inputs)
         {
+            
+            
             var sum = 0.0;
             for (var i = 0; i < inputs.Count; i++)
             {
                 sum += Weights[i] * inputs[i];
+                //сохраняем входные сигналы
+                Inputs[i] = inputs[i];
             }
             //Для входных нейронов сигмоидная функция не применяется
             Output = NeuralType != NeuralType.Input ? Sigmoid(sum) : sum;
@@ -38,6 +55,12 @@ namespace NeuralNetworks
             return result;
         }
 
+        private double SigmoidDx(double x)
+        {
+            var sigmoid = Sigmoid(x);
+            return sigmoid / (1-sigmoid);
+        }
+
         public void SetWeights(params double[] weights)
         {
            //todo проверить соотвествие количекства весов и входов у нейрона 
@@ -47,8 +70,23 @@ namespace NeuralNetworks
            {
                Weights[i] = weights[i];
            }
-           //Weights.AddRange(weights);
+           
         }
+
+        public void Lern(double error, double learningRate)
+        {
+            //входные нейроны не обучаются
+            if (NeuralType == NeuralType.Input) return;
+            Delta = error * SigmoidDx(Output);
+            for (var i = 0; i < Weights.Count; i++)
+            {
+                var weight = Weights[i];
+                var input = Inputs[i];
+                var newWeight = weight - input * Delta * learningRate;
+                Weights[i] = newWeight;
+            }
+        }
+        
         
         public override string ToString()
         {
