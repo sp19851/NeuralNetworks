2c618aa2fdb4ab1f7af3d4d1e4b2b009dfb0a6db
sp19851 <sp19851@gmail.com>
2025-10-26 07:46:33 +0200

    Добавили lerningRate в топологию

--- NeuralNetworks/Topology.cs
+++ NeuralNetworks/Topology.cs
@@ -6,13 +6,17 @@ namespace NeuralNetworks
     {
         public int InputCount { get; }
         public int OutputCount { get; }
-        //коллекция количества нейронов на каждом скрытом слое. Скрытый это который ни входной, ни выходной 
+
+        public double LerningRate { get; }
+
+        //Коллекция количества нейронов на каждом скрытом слое. Скрытый это который ни входной, ни выходной 
         public List<int> HiddenLayers { get; }
 
-        public Topology(int  inputCount, int outputCount, params int[] hiddenLayers)
+        public Topology(int  inputCount, int outputCount, double lerningRate, params int[] hiddenLayers)
         {
             InputCount = inputCount;
             OutputCount = outputCount;
+            LerningRate = lerningRate;
             HiddenLayers = new List<int>(hiddenLayers);
         }
     }
